f05b46d795e20f3e86dfdfccd81a129fd93a6157
Shantanu Mahato <62724719+ShanuXD@users.noreply.github.com>
2020-11-27 02:19:29 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 from turtle import Turtle
 
+
 class Scoreboad(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.score=0
+        self.score = 0
+        with open('data.txt') as file:
+            self.high_score = int(file.read())
+
         self.color('white')
         self.penup()
         self.goto(0, 260)
@@ -12,15 +16,25 @@ class Scoreboad(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+             
+            with open('data.txt',mode='w') as file:
+                file.write(f'{self.high_score}')
+
+        self.score = 0
         self.update_scoreboard()
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over",align="center", font=("Arial", 24, "normal"))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over",align="center", font=("Arial", 24, "normal"))
 
 
