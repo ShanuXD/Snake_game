ab7da1edd95d226afc337948bfd94d8d0edcb1ec
Shantanu Mahato <62724719+ShanuXD@users.noreply.github.com>
2020-11-27 02:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,27 +30,21 @@ while game_is_on:
     snake.move()
 
     #collision with food
-
     if snake.head.distance(food)<15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
     #colllsion with side
-
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on=False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #collision with tail
-
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on=False
-            scoreboard.game_over()
-
-
-
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
